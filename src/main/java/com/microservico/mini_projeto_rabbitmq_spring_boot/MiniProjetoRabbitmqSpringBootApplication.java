8967fcc9d82d4c8e07523a85dcbc33f5747ae283
Yves <yves.silva@itixti.com>
2024-09-10 08:46:31 -0300

    estrutura

---
+++ src/main/java/com/microservico/mini_projeto_rabbitmq_spring_boot/MiniProjetoRabbitmqSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.microservico.mini_projeto_rabbitmq_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjetoRabbitmqSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjetoRabbitmqSpringBootApplication.class, args);
+	}
+
+}
